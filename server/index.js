870acb19f44e244ccb3cbfb24af9c57323a95028
Logan Young <logany@att.net>
2021-11-17 16:12:42 -0600

    for

--- server/index.js
+++ server/index.js
@@ -7,14 +7,15 @@ app.get('/',function(req,res) {
   res.sendFile(path.join(__dirname, '../index.html'));
 });
 
-app.get('/js', (req, res) => {
-    res.sendFile(path.join(__dirname, './client/main.js'))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/main.js'))
   })
-const port = process.env.PORT || 4004
-
+  
 app.get('/animals', (req, res) => {
     res.send(['Lions', 'Tigers', 'Bears'])
 })
+    
+const port = process.env.PORT || 4004
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
